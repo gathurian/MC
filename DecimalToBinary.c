dfa572fca386f4a2bc0abd58c64a04d7ae2b1eba
Alex <mainzel@windowslive.com>
2018-03-06 17:32:03 +0100

    Added DecimalToBinary and MenuChooser (unfinished)

---
+++ DecimalToBinary.c
@@ -0,0 +1,31 @@
+//
+// Created by Alex on 01.03.2018.
+//
+#include <stdio.h>
+
+void DecimalToBinary(int decimal){
+    int i;
+    int shift;
+
+    for (i = 31; i >= 0; i--)
+    {
+        shift = decimal >> i;
+
+        if (shift & 1)
+            printf("1");
+        else
+            printf("0");
+    }
+
+    printf("\n");
+}
+
+int main(){
+    for(;;){
+        int wert;
+        printf("Wert als Integer (auch negativ): ");
+        scanf("%d", &wert);
+        DecimalToBinary(wert);
+    }
+}
+
